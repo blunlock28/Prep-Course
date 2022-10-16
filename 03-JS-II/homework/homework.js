d62920ef466a19dd105193bbf4364db9ee74ecd8
blunlock28 <blunlock28@gmail.com>
2022-10-16 17:26:28 -0400

    tarea numero 2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -219,7 +219,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if(numero >=100 && numero < 999){
+  if(numero >=100 && numero <= 999){
   return true
   }
   return false
